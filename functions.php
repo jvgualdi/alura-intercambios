6eacf9d9c263182b926d9e11df8f8c70fde32bbd
José Vitor Gualdi <jvgualdi1@gmail.com>
2022-03-29 23:55:23 -0300

    Configuring header

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+
+function alura_intercambios_adding_theme_resources(){
+    add_theme_support('custom-logo');
+}
+add_action('after_setup_theme', 'alura_intercambios_adding_theme_resources');
+
+function alura_intercambios_registration_menu(){
+    register_nav_menu('menu-navegacao', 'Menu Navegação');
+}
+
+add_action('init', 'alura_intercambios_registration_menu');
---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, maximum-scale=1.0, minimum-scale=1.0">
+    <title><?php bloginfo('name'); ?></title>
+    <?php wp_head(); ?>
+    <link rel="stylesheet" href="<?= get_template_directory_uri() . '/css/normalize.css'?>">
+    <link rel="stylesheet" href="<?= get_template_directory_uri() . '/css/bootstrap.css'?>">
+    <link rel="stylesheet" href="<?= get_template_directory_uri() . '/css/header.css'?>">
+</head>
+<body>
+<?php
+    the_custom_logo();
+
+    wp_nav_menu(
+        array('menu' => 'menu-navegacao')
+    );
+
