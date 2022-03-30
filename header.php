1cdeab3175428bda6b8c8580edcdd236499491d6
José Vitor Gualdi <jvgualdi1@gmail.com>
2022-03-30 11:08:49 -0300

    header configuration to use correct classes

--- header.php
+++ header.php
@@ -10,11 +10,22 @@
     <link rel="stylesheet" href="<?= get_template_directory_uri() . '/css/bootstrap.css'?>">
     <link rel="stylesheet" href="<?= get_template_directory_uri() . '/css/header.css'?>">
 </head>
-<body>
+<body <?php body_class(); ?>>
+<header class="site-header">
+    <div class="container-alura">
+        <?php
+            the_custom_logo();
+        ?>
+        <nav>
+            <?php
+                 wp_nav_menu(
+                    array('menu' => 'menu-navegacao', 'menu_id' => 'menu-principal')
+                );            
+            ?>
+        </nav>
+    </div>
+</header>
 <?php
-    the_custom_logo();
+    
 
-    wp_nav_menu(
-        array('menu' => 'menu-navegacao')
-    );
-
+   
