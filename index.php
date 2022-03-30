3b469c40fed34993bde5138ae3aca4ab2c880980
José Vitor Gualdi <jvgualdi1@gmail.com>
2022-03-30 13:02:54 -0300

    Configured and styled the 'about us' content

--- index.php
+++ index.php
@@ -1,8 +1,22 @@
 <?php
+$estiloPagina = 'sobre_nos.css';
 require_once 'header.php';
-?>
 
-<h1> Olá Mundo!  </h1>
+if(have_posts()){
+    ?>
+    <main class="main-sobre-nos">
+        <?php
+        while(have_posts()){
+            the_post();
+            the_post_thumbnail('post-thumbnail', array('class'=>'imagem-sobre-nos'));
+            echo '<div class="conteudo container-alura">';
+            the_title('<h2>', '</h2>');
+            the_content();
+            echo '</div>';
+        }
+        ?>
+    </main>
+<?php
+}
 
-<?php 
 require_once 'footer.php';
